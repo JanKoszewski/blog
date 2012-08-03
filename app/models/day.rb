a21be52440d3c7aa0b6bde32efb772e3a5c5839c
jkosz <jan.koszewski@livingsocial.com>
2012-08-03 16:51:25 -0400

    creating model relationships

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,3 +1,5 @@
 class Day < ActiveRecord::Base
   # attr_accessible :title, :body
+  has_many :meals
+  has_many :workouts
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,3 +1,4 @@
 class Workout < ActiveRecord::Base
   # attr_accessible :title, :body
+  belongs_to :day
 end
